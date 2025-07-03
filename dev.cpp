baf9a9f7e3bb1dc29e3b24779f7010cdbd949dab
khurram1238 <khurram132khan@gmail.com>
2025-07-03 09:10:27 +0500

    Add files via upload

---
+++ dev.cpp
@@ -0,0 +1,7 @@
+// Development version with extra logging
+#include <iostream>
+int main() {
+    std::cout << "OS Simulator - Development Version\n";
+    std::cout << "Initializing Memory Manager...\n";
+    return 0;
+}
